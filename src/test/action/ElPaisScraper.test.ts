f27e748f05cce05fa25fce2244ed53efbe91fee8
Jesus <llius619@gmail.com>
2022-04-10 23:25:56 +0200

    test(2-2-2-4-5): Add test to dont save notices from diferent date

--- src/test/action/ElPaisScraper.test.ts
+++ src/test/action/ElPaisScraper.test.ts
@@ -82,40 +82,36 @@ describe("ElPaisScraper", () => {
       )
     );
   });
-  //   it(`
-  //       GIVEN i want to scrape el mundo
-  //       WHEN i scrape El mundo with specfic date
-  //       THEN i should save news from the specfic date
-  //   `, async () => {
-  //     // WHEN
-  //     const idGenerator = new IdGeneratorMock();
-  //     jest.spyOn(idGenerator, "run").mockImplementation(() => {
-  //       return "62529656d717a077bec16624";
-  //     });
+  it(`
+        GIVEN i want to scrape "ElPais"
+        WHEN i scrape "ElPais" with specfic date
+        THEN i should save news from the specfic date
+    `, async () => {
+    // WHEN
+    const idGenerator = new IdGeneratorMock();
+    jest.spyOn(idGenerator, "run").mockImplementation(() => {
+      return "62529656d717a077bec16624";
+    });
 
-  //     const elMundoRepoMock = new NoticeRepoMock();
-  //     const elMundoRepoMockSpy = jest.spyOn(elMundoRepoMock, "save");
+    const elPaisRepoMock = new NoticeRepoMock();
+    const elPaisRepoMockSpy = jest.spyOn(elPaisRepoMock, "save");
 
-  //     const scraperMock = new ScraperMock();
-  //     jest.spyOn(scraperMock, "run").mockImplementation(async () => {
-  //       return [
-  //         {
-  //           url: "https://www.elmundo.es/internacional/2011/01/01/6251c2a9e4d4d843778b459d.html",
-  //           title:
-  //             'La guerra rompe los lazos prorrusos en el este de Ucrania: "No creo que nunca les perdonemos, es imposible"',
-  //         },
-  //       ];
-  //     });
-  //     jest.useFakeTimers().setSystemTime(new Date("2022-04-09"));
-  //     const action = new ElMundoScrapper(
-  //       elMundoRepoMock,
-  //       scraperMock,
-  //       idGenerator
-  //     );
-  //     await action.run();
+    const scraperMock = new ScraperMock();
+    jest.spyOn(scraperMock, "run").mockImplementation(async () => {
+      return [
+        {
+          title:
+            "De Jong marca en los últimos minutos para el Barça ante el Levante (2-3)",
+          url: "/deportes/2011-01-01/levante-barcelona-en-directo-la-liga-santander-en-vivo.html",
+        },
+      ];
+    });
+    jest.useFakeTimers().setSystemTime(new Date("2022-04-09"));
+    const action = new ElPaisScrapper(elPaisRepoMock, scraperMock, idGenerator);
+    await action.run();
 
-  //     expect(elMundoRepoMockSpy).not.toBeCalled();
-  //   });
+    expect(elPaisRepoMockSpy).not.toBeCalled();
+  });
   //   it(`
   //     GIVEN i want to scrape el mundo
   //     WHEN i scrape El mundo and get news that are already on repo
